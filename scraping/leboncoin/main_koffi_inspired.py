afe2542189d80a9b62d6f18d85e92f745ed8aebc
matthieuneau <matthieu.neau@student-cs.fr>
2024-01-21 00:23:54 +0100

    successful script to retrieve all urls on a page. Now time to combine this code with the utils functions that retrieve the data at the url

--- scraping/leboncoin/main_koffi_inspired.py
+++ scraping/leboncoin/main_koffi_inspired.py
@@ -1,24 +1,27 @@
-import undetected_chromedriver as uc 
+import undetected_chromedriver as uc
 import os
 import pandas as pd
 from bs4 import BeautifulSoup
 import time
 from selenium.webdriver.common.by import By
 
-## Set Chrome Options
+# Set Chrome Options
 options = uc.ChromeOptions()
 # options.headless=True
 # options.add_argument('--headless')
 # options.add_argument(f'--proxy-server={PROXY}')
 # options.add_argument(f"user-agent={my_user_agent}")
 
-## Create Undetected Chromedriver with Proxy
+# Create Undetected Chromedriver with Proxy
 driver = uc.Chrome(options=options)
 
 url = "https://www.leboncoin.fr/f/locations/real_estate_type--2"
-## Send Request
+# Send Request
 driver.get(url)
-annonces = driver.find_element(By.XPATH, "//a[@data-test-id='ad' and @data-qa-id='aditem_container']")
+
+time.sleep(4)
+
+annonces = driver.find_elements(By.XPATH, "//a[@data-test-id='ad' and @data-qa-id='aditem_container']")
 
 liste_liens = []
 # Récupérez les liens des annonces
