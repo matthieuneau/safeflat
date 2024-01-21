7ddbde04e0fab5c83a4d3a6a31e858d2c190cd20
matthieuneau <matthieu.neau@student-cs.fr>
2024-01-21 19:37:53 +0100

    edited the main so that it retrieves the urls of all posts on result search page and then calls retrieve_data on them

--- scraping/pap/main.py
+++ scraping/pap/main.py
@@ -6,7 +6,8 @@ from selenium.webdriver.common.by import By
 import utils
 
 # URL of the flat
-url = "https://www.pap.fr/annonces/appartement-alfortville-94140-r441800902"
+# url = "https://www.pap.fr/annonces/appartement-alfortville-94140-r441800902"
+url = "https://www.pap.fr/annonce/location-appartement-maison"
 
 # Setup Chrome options for headless browsing
 options = Options()
@@ -14,4 +15,41 @@ options = Options()
 # Initialize the WebDriver with the specified options
 driver = uc.Chrome(options=options)
 
-utils.retrieve_data(url, driver, "output.csv")
+# utils.retrieve_data(url, driver, "output.csv")
+
+driver.get(url)
+
+time.sleep(3)
+
+# last_height = driver.execute_script("return document.body.scrollHeight")
+
+# while True:
+#     # Scroll down to the bottom of the page
+#     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+#     # Wait for new content to load
+#     time.sleep(2)  # Adjust the sleep time as necessary
+
+#     # Calculate new scroll height and compare with last scroll height
+#     new_height = driver.execute_script("return document.body.scrollHeight")
+#     if new_height == last_height:
+#         break
+#     last_height = new_height
+
+# time.sleep(3)
+
+url_elements = driver.find_elements(By.CSS_SELECTOR, ".item-title")
+urls = [
+    element.get_attribute("href")
+    for element in url_elements
+    if element.get_attribute("href").startswith(
+        "https://www.pap.fr/annonces"
+    )  # avoids to retrieve the urls that redirect to ads
+]
+# print(urls)
+
+for url in urls:
+    utils.retrieve_data(url, driver, "output.csv")
+    time.sleep(2)
+
+driver.quit()
