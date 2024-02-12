f15dd6c1bdd73d373582dae5e82afdf0fced4bd1
Matthieu Neau <mneau@Matthieus-MacBook-Pro.local>
2024-02-13 00:54:34 +1100

    almost done with processing lbc webpage. Just need to take care of criteres

--- scraping/leboncoin/main.py
+++ scraping/leboncoin/main.py
@@ -2,42 +2,79 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import sys
 import time
+import yaml
+from bs4 import BeautifulSoup
+import undetected_chromedriver as uc
+from selenium.webdriver.common.by import By
 
-sys.path.append(
-    r"C:\Users\mneau\OneDrive\Bureau\INFO\PYTHON\selenium\chromedriver_1.120.6099.129"
-)
+with open("../../config.yaml", "r") as file:
+    config = yaml.safe_load(file)
 
-# Chrome options
-options = Options()
-options.add_argument(
-    "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
-)
+# Setup Chrome options for undetected_chromedriver
+options = uc.ChromeOptions()
+options.add_argument("--headless")
+# options.add_argument("--incognito")
 
-# Create a new instance of the Chrome driver
-driver = webdriver.Chrome(
-    r"C:\Users\mneau\OneDrive\Bureau\INFO\PYTHON\selenium\chromedriver_1.120.6099.129\chromedriver.exe"
-)
+# Initialize the WebDriver with the specified options
+driver = uc.Chrome(options=options)
 
 # URL of the page
-# url = "https://www.leboncoin.fr/offre/locations/2467639678"
-url = "https://www.leboncoin.fr/"
-
+url = "file:///Users/mneau/Desktop/safeflat/scraping/leboncoin/leboncoin_example.html"
 
 # Navigate to the page
 driver.get(url)
 
 # Wait to bluff the captcha mechanism
-time.sleep(5)
+time.sleep(2)
 
-# Find the paragraph element
-paragraph = driver.find_element_by_css_selector("p.whitespace-pre-line")
+title = driver.find_element(
+    By.CSS_SELECTOR, ".break-words.text-headline-1-expanded.undefined"
+).text
+print(f"title: {title}")
 
-# Retrieve the text
-text = paragraph.text
+specs = driver.find_elements(
+    By.CSS_SELECTOR, ".inline-flex.w-full.flex-wrap.mb-md span"
+)
+specs = [spec.text for spec in specs]
+print(f"specs: {specs}")
 
-# Write the text to a file
-with open("output.txt", "w", encoding="utf-8") as file:
-    file.write(text)
+# Retrieving price
+price = driver.find_element(
+    By.XPATH, '//*[@id="grid"]/article/div[1]/div/div[1]/div[2]/div/p'
+).text
+print(f"price: {price}")
+
+# Retrieving post date
+post_date = driver.find_element(
+    By.XPATH,
+    '//*[@id="grid"]/article/div[1]/div/div[2]/p',
+).text
+print(f"post_date: {post_date}")
+
+# Retrieving description
+description = driver.find_element(
+    By.XPATH,
+    '//*[@id="grid"]/article/div[4]/div[2]/div/p',
+).text
+print(f"description: {description}")
+
+# Retrieving criteres
+criteres = driver.find_elements(
+    By.CSS_SELECTOR, "div.styles_criteria__U5Ul8.flex.flex-wrap > div > div"
+)
+
+print(criteres[0].find_element(By.CSS_SELECTOR, "span").text)
+
+
+criteres = {
+    c.find_element(By.CSS_SELECTOR, "p")
+    .text: c.find_element(By.CSS_SELECTOR, "span")
+    .text
+    for c in criteres
+}
+print(f"criteres: {criteres}")
 
 # Close the browser
 driver.quit()
+
+# grid > article > div:nth-child(2) > div > div.sc-12a6ec0d-0.gJGwBE > div.mr-md.flex.flex-wrap.items-center.justify-between > div > p
