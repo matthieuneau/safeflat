ba32d90e69caa8e68a54999dc342b27ce0988e50
Matthieu Neau <mneau@Matthieus-MacBook-Pro.local>
2024-02-12 22:07:59 +1100

    getting through retrieving everything from a pap webpage

--- scraping/pap/main.py
+++ scraping/pap/main.py
@@ -6,12 +6,15 @@ import yaml
 import utils
 from bs4 import BeautifulSoup
 import undetected_chromedriver as uc
+from selenium.webdriver.common.by import By
+
 
 with open("../../config.yaml", "r") as file:
     config = yaml.safe_load(file)
 
 # Setup Chrome options for undetected_chromedriver
 options = uc.ChromeOptions()
+options.add_argument("--headless")
 
 # Initialize the WebDriver with the specified options
 driver = uc.Chrome(options=options)
@@ -24,17 +27,46 @@ driver.get(url)
 # Wait for the element to be loaded
 time.sleep(2)
 
+# Create a dictionary to store the data we scrape
+data = {"title": [], "price": [], "description": []}
+
 # Find the paragraph element using CSS Selector
-paragraph = driver.find_element_by_css_selector(
-    ".ll4r2nl.atm_kd_pg2kvz_1bqn0at.dir.dir-ltr"
+title_and_price = driver.find_element(
+    By.XPATH, "/html/body/div[2]/div/div[1]/h1[@class='item-title']"
 )
+title_and_price = title_and_price.text
+print(f"title_and_price: {title_and_price}")
 
-# Retrieve the text
-text = paragraph.text
+# Retrieving location
+location = driver.find_element(By.XPATH, "/html/body/div[2]/div/div[1]/div[5]/h2")
+location = location.text
+print(f"location: {location}")
 
-# Write the text to a file
-with open("output.txt", "w", encoding="utf-8") as file:
-    file.write(text)
+# Retrieving nb of rooms, surface and nb of bedrooms when available
+list_items = driver.find_elements(
+    By.CSS_SELECTOR,
+    "body > div.sidebar-layout.margin-top-header.details-annonce-container > div > div.main-content.details-item.padding-top-30.padding-bottom-60.margin-bottom-60.sm-padding-bottom-40.sm-margin-bottom-30 > div.item-description.margin-bottom-30 > ul.item-tags.margin-bottom-20 > li",
+)
+details = [item.find_element(By.TAG_NAME, "strong").text for item in list_items]
+print(f"details: {details}")
 
-# Close the browser
+# Retrieving description
+description = driver.find_element(
+    By.XPATH, "/html/body/div[2]/div/div[1]/div[5]/div[1]"
+)
+description = description.text
+print(f"description: {description}")
+
+# Retrieving metro stations closeby
+metro = driver.find_elements(
+    By.CSS_SELECTOR,
+    ".item-transports",
+)
+metro_stations = []
+metro_stations = [item.text for item in transport]
+print(f"metro_stations: {metro_stations}")
+
+conditions_financieres = driver.find_elements(By.CSS_SELECTOR, ".row > .col-1-3")
+conditions_financieres = [item.text for item in conditions_financieres]
+
 driver.quit()
