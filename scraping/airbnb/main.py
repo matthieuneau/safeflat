50fce958bd93846f54145259c27099a9f64f4f25
koffipynb <hennecon.lucas@gmail.com>
2024-02-28 21:28:57 +1000

    Preprocessing du csv d'airbnb

--- scraping/airbnb/main.py
+++ scraping/airbnb/main.py
@@ -7,61 +7,66 @@ import utils
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
+import preprocessing
 
 
 
 
-# page_nb = 2
-urls = []
-url = "https://www.airbnb.fr/s/Tourcoing--France/homes?tab_id=home_tab&refinement_paths%5B%5D=%2Fhomes&flexible_trip_lengths%5B%5D=one_week&monthly_start_date=2024-03-01&monthly_length=3&monthly_end_date=2024-06-01&price_filter_input_type=0&channel=EXPLORE&date_picker_type=calendar&checkin=2024-04-11&checkout=2024-04-12&source=structured_search_input_header&search_type=autocomplete_click&price_filter_num_nights=1&query=Tourcoing%2C%20France&place_id=ChIJXScKvtQow0cRj4WtfX0xLiU&adults=8"
-# Setup Chrome options for undetected_chromedriver
-options = uc.ChromeOptions()
+# # page_nb = 2
+# urls = []
+# url = "https://www.airbnb.fr/s/Tourcoing--France/homes?tab_id=home_tab&refinement_paths%5B%5D=%2Fhomes&flexible_trip_lengths%5B%5D=one_week&monthly_start_date=2024-03-01&monthly_length=3&monthly_end_date=2024-06-01&price_filter_input_type=0&channel=EXPLORE&date_picker_type=calendar&checkin=2024-04-11&checkout=2024-04-12&source=structured_search_input_header&search_type=autocomplete_click&price_filter_num_nights=1&query=Tourcoing%2C%20France&place_id=ChIJXScKvtQow0cRj4WtfX0xLiU&adults=8"
+# # Setup Chrome options for undetected_chromedriver
+# options = uc.ChromeOptions()
 
-# Initialize the WebDriver with the specified options
-driver = uc.Chrome(options=options)
+# # Initialize the WebDriver with the specified options
+# driver = uc.Chrome(options=options)
 
-driver.get(url)
+# driver.get(url)
 
-try:
-    while True:  # Boucle jusqu'à ce qu'on ne trouve plus de lien "Suivant"
-        # Attendre que le lien "Suivant" soit chargé et cliquable
-        wait = WebDriverWait(driver, 120)
-        wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '[rel="noopener noreferrer nofollow"]')))
+# try:
+#     while True:  # Boucle jusqu'à ce qu'on ne trouve plus de lien "Suivant"
+#         # Attendre que le lien "Suivant" soit chargé et cliquable
+#         wait = WebDriverWait(driver, 120)
+#         wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '[rel="noopener noreferrer nofollow"]')))
 
-        url_elements = driver.find_elements(By.CSS_SELECTOR, '[rel="noopener noreferrer nofollow"]')
-        urls_temp = [
-            element.get_attribute("href")
-            for element in url_elements
-            if element.get_attribute("href").startswith(
-                "https://www.airbnb.fr/rooms/"
-            )  # avoids to retrieve the urls that redirect to ads
-        ]
+#         url_elements = driver.find_elements(By.CSS_SELECTOR, '[rel="noopener noreferrer nofollow"]')
+#         urls_temp = [
+#             element.get_attribute("href")
+#             for element in url_elements
+#             if element.get_attribute("href").startswith(
+#                 "https://www.airbnb.fr/rooms/"
+#             )  # avoids to retrieve the urls that redirect to ads
+#         ]
 
-        urls_temp = list(set(urls_temp))
-        urls+= urls_temp
-        next_link = WebDriverWait(driver, 30).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, 'a[aria-label="Suivant"]'))
-        )
+#         urls_temp = list(set(urls_temp))
+#         urls+= urls_temp
+#         next_link = WebDriverWait(driver, 30).until(
+#             EC.presence_of_element_located((By.CSS_SELECTOR, 'a[aria-label="Suivant"]'))
+#         )
         
-         # Cliquer sur le lien "Suivant"
-        link_element= driver.find_element(By.CSS_SELECTOR, 'a[aria-label="Suivant"]')
-        link_url = link_element.get_attribute('href')
+#          # Cliquer sur le lien "Suivant"
+#         link_element= driver.find_element(By.CSS_SELECTOR, 'a[aria-label="Suivant"]')
+#         link_url = link_element.get_attribute('href')
 
-        driver.get(link_url)
+#         driver.get(link_url)
 
 
-        # Optionnel : Opérations supplémentaires sur la page courante avant de passer à la suivante
+#         # Optionnel : Opérations supplémentaires sur la page courante avant de passer à la suivante
         
-except Exception as e:
-    # Si le lien "Suivant" n'est pas trouvé, on suppose qu'on est à la dernière page
-    print("Dernière page atteinte.")
+# except Exception as e:
+#     # Si le lien "Suivant" n'est pas trouvé, on suppose qu'on est à la dernière page
+#     print("Dernière page atteinte.")
 
-finally:
-    driver.quit() 
+# finally:
+#     driver.quit() 
 
 
-#for url in urls:
-for i in range(10,15):  
-    utils.retrieve_data(urls[i], "output.csv")
-    time.sleep(2)
+# #for url in urls:
+# for i in range(15,20):  
+#     utils.retrieve_data(urls[i], "output.csv")
+#     time.sleep(2)
+
+
+#Preprocess the csv file:
+preprocessing.preprocess_csv('output.csv', 'output_preprocess.csv')
 
---
+++ scraping/airbnb/preprocessing.py
@@ -0,0 +1,69 @@
+from collections import Counter
+import re
+import pandas as pd
+
+file_path = 'output.csv'
+data = pd.read_csv(file_path)
+
+# Fonction pour extraire les chiffres
+def extract_number(text):
+    if pd.isnull(text):
+        return 0
+    numbers = re.findall(r'\d+', text)
+    return int(numbers[0]) if numbers else 0
+
+# Fonction pour compter les lits
+def count_beds(bed_str):
+    if pd.isna(bed_str):
+        return [0, 0, 0, 0, 0]
+    
+    # Normaliser les pluriels en singuliers pour comptage
+    normalized_str = bed_str.replace('lits doubles', 'lit double')\
+                             .replace('canapés convertibles', 'canapé convertible')\
+                             .replace('lits simples', 'lit simple')\
+                             .replace('canapés', 'canapé')\
+                             .replace('lits superposés', 'lit superposé')
+    
+    # Trouver tous les lits et leurs quantités
+    beds = re.findall(r'(\d+) (lit double|canapé convertible|lit simple|canapé|lit superposé)', normalized_str)
+    
+    # Compter les lits par type
+    bed_counts = Counter({key: 0 for key in ['lit double', 'canapé convertible', 'lit simple', 'canapé', 'lit superposé']})
+    for count, bed_type in beds:
+        bed_counts[bed_type] += int(count)
+    
+    return [bed_counts['lit double'], bed_counts['canapé convertible'], bed_counts['lit simple'], bed_counts['canapé'], bed_counts['lit superposé']]
+
+def preprocess_csv(input_file_path, output_file_path):
+    # Charger le fichier CSV
+    data = pd.read_csv(input_file_path)
+
+    # Prétraitement des colonnes avec extraction des chiffres
+    data['nb_voyageurs'] = data['nb_voyageurs'].apply(extract_number)
+    data['nb_sdb'] = data['nb_sdb'].apply(lambda x: extract_number(x.split(' ')[0]))
+    data['price'] = data['price'].apply(lambda x: extract_number(x.replace('€', '').replace('\xa0', '').replace(' ', '')))
+    data['nb_bedrooms'] = data['nb_bedrooms'].apply(lambda x: extract_number(x.split(' ')[0]))
+
+    # Gérer la colonne nb_rooms si elle existe
+    if 'nb_rooms' in data.columns:
+        data['nb_rooms'] = data['nb_rooms'].apply(extract_number)
+    else:
+        data['nb_rooms'] = 0  # Ajouter une colonne nb_rooms avec des zéros si elle n'existe pas
+
+    # Compter les types de lits
+    bed_types = data['nb_beds'].apply(count_beds)
+    bed_columns = ['nb_lits_doubles', 'nb_canapes_convertibles', 'nb_lits_simples', 'nb_canapes', 'nb_lit_superposes']
+    for idx, col in enumerate(bed_columns):
+        data[col] = [beds[idx] for beds in bed_types]
+
+    # Sélectionner les colonnes nécessaires pour le fichier de sortie
+    columns_to_keep = ['title', 'host_name', 'equipements', 'description', 'nb_rooms', 'numero_etage', 'surface', 'price', 'nb_bedrooms'] + bed_columns
+    processed_data = data[columns_to_keep]
+
+    # Sauvegarder dans un nouveau fichier CSV
+    processed_data.to_csv(output_file_path, index=False)
+
+
+
+
+
