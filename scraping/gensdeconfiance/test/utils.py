0f93bd834e7ae88106440e91cd6f332a4615fe0a
Matthieu Neau <mneau@Matthieus-MacBook-Pro.local>
2024-03-04 23:39:17 +1100

    adding chatgpt to process descriptions

--- scraping/gensdeconfiance/test/utils.py
+++ scraping/gensdeconfiance/test/utils.py
@@ -1,8 +1,11 @@
+import os
 import random
 import time
 import yaml
 from selenium.webdriver.common.by import By
 import undetected_chromedriver as uc
+from langchain_core.messages import HumanMessage
+from langchain_openai import OpenAI
 
 
 def get_annonce_data(driver, annonce):
@@ -103,52 +106,67 @@ def get_annonce_data(driver, annonce):
     return data
 
 
-"""
-    # Retrieving title
-    title = driver.find_element(By.CSS_SELECTOR, "h1#post-title").text
-    print(f"title: {title}")
+def process_description(description):
+    """
+    Processes the description of an annonce.
+    """
+    os.environ["OPENAI_API_KEY"] = "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"
+    llm = OpenAI()
 
-    # Retrieving subtitle
-    subtitle = driver.find_element(By.CSS_SELECTOR, "h2#post-title-breadcrumb").text
-    print(f"subtitle: {subtitle}")
+    question = f"""Tu es un expert en location immobilière et tu maitrises tout le vocabulaire associe. Tu dois m’aider a extraire des informations pertinentes parmi de longues descriptions de biens immobiliers que je vais te donner.
 
-    # Retrieving price titles
-    titles = driver.find_elements(
-        By.CSS_SELECTOR, "div.price-table > div.price-table__row > div.price-table__cell"
-    )
-    titles_list = [item.text for item in titles]
+    Voici la description d'une annonce d'un bien immobilier qui est mis en location sur un site d'annonces. Essaie de relever les informations suivantes dans le texte de cette description:
 
-    # Retrieving price values
-    values = driver.find_elements(
-        By.CSS_SELECTOR, "div.price-table > div.price-table__row > div.price-table__value"
-    )
-    values_list = [item.text for item in values]
+    - surface: La surface indiquée en m2
+    - nombre de pieces: Le nombre de pieces 
+    - piscine: La presence ou non d’une piscine. Renvoie oui si elle est présente, non sinon. Une piscine sera toujours indiquée explicitement dans la description donc si elle n’est pas indiquée, renvoie non
+    - type de bien: Le type de bien dont il s’agit. Il peut uniquement s’agir d’un appartement ou d’une maison
+    - nombre de chambres: Le nombre de chambres
+    - parking: La presence ou non d’une place de parking privée. Renvoie oui si elle est présente, non sinon
+    - quartier: Le nom du quartier ou est situe le bien immobilier
+    - meuble: Si le bien est meuble renvoie oui, sinon renvoie non
+    - nombre d’etages: Le nombre d’etages du bien
+    - numero d’etage: A quel étage se situe le bien s’il s’agit d’un appartement 
+    - ascenseur:La presence ou non d’un ascenseur qui permet d’acceder a l’appartement s’il s’agit d’un appartement en immeuble. Renvoie oui s’il y a un ascenseur, non sinon
+    - cave: La presence ou non d’une cave. Renvoie oui s’il y a une cave, non sinon
+    - terrasse: La presence ou non d’une terrasse. Renvoie oui s’il y en a une, non sinon
 
-    # Zipping titles and values
-    prices = dict(zip(titles_list, values_list))
-    print(f"prices: {prices}")
 
-    # Retrieving characteristics titles
-    characteristics_titles = driver.find_elements(
-        By.CSS_SELECTOR, "div#sfreact-reactRenderer65d1549745f563\.19298540 li > p"
-    )
-    titles = [item.text for item in characteristics_titles]
+    Renvoie un simple dictionnaire qui les contient. N’essaie pas d’inventer ou de deviner des informations. A chaque fois que tu ne trouves pas une information, associe la valeur “N/A” a la clé. Par exemple, si tu n’es pas en mesure de determiner si le bien est meuble ou non, inclu “meuble”: N/A dans le dictionnaire.
+    Voici quelques exemples pour t’aider:
 
-    # Retrieving characteristics values
-    characteristics_values = driver.find_elements(
-        By.CSS_SELECTOR, "div#sfreact-reactRenderer65d1549745f563\.19298540 li li>span"
-    )
-    values = [item.text for item in characteristics_values]
+    Exemple 1:
+    F3 Bien situé dans les hauts de Sainte-Suzanne deux rives proche de toutes commodités. Une cuisine ouverte et deux chambres plus 2 salles de bains et wc en bas et en haut.l’entrée donne directement sur une terrasse sécurisée à l’arrière une autre terrasse donnait directement sur une courette. Cette location meublée d'un montant 1150€mois tout charge inclus de plus il n’y a pas tout à l’égout. L’ axé via un portail électrique donnant dans une cour privée idéal pour seniors cherchant la tranquillité
 
-    # Zipping titles and values
-    characteristics = dict(zip(titles, values))
-    print(f"characteristics: {characteristics}")
+    Reponse 1:
+    {{"surface": "N/A”, "nombre de pieces": 3, "piscine": "Non", "type de bien": "N/A", "nombre de chambres": "N/A", "parking": "N/A", "quartier": "N/A", "meuble": "N/A", "nombre d’etages": "N/A", "numero d’etage": "N/A", "ascenseur": "N/A", "cave": "N/A", "terrasse": "oui"}}
 
-    # Retrieving description
-    description = driver.find_element(By.CSS_SELECTOR, "div#ad-description").text
-    print(f"description: {description}")
+    Exemple 2:
+    - Quartier calme et résidentiel.
+    - Portail et entrée individuelle, cuisine semi-équipée, salle de bain avec baignoire et toilettes séparées avec lave-mains.
+    - Suite parentale (chambre avec salle d'eau).
+    - Appartement au premier étage dans une grande maison.
+    - Deux terrasses dans un appartement traversant : possibilité de manger à l'extérieur.
+    - Parking individuel.
+    - Placards de rangements dans deux chambres et à l'entrée.                                                                                                                                                                               
+    - Proximité des écoles : maternelle, élémentaire, collège et lycée.
+    - Proximité des transports en commun : lignes de bus 2, 4 et 5 au bout de la rue, RER A gare Sucy-Bonneuil accessible en 10min de bus ou 25min à pied.
+    - Commerces et équipements sportifs à proximité.
+    - Parc Municipal des Sports de Sucy à moins d'un kilomètre : club de tennis, football, athlétisme, rugby, stade, etc.
+    - Conservatoire de musique à proximité.
+    - Seule charge : taxe d'ordure ménagère.                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              "['Sucy - Bonneuil', 'Boissy-Saint-Léger', 'La Varenne - Chennevières']"                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
 
-    # Write the text to a file
-    # with open("output.txt", "w", encoding="utf-8") as file:
-    #     file.write(text)
-"""
+    Reponse 2:
+    {{"surface": "N/A", "nombre de pieces": "N/A", "piscine": "Non", "type de bien": "appartement", "nombre de chambres": "N/A", "parking": "oui", "quartier": "N/A", "meuble": "N/A", "nombre d’etages": "N/A", "numero d’etage": 1, "ascenseur": "N/A", "cave": "N/A", "terrasse": "oui"}} 
+
+
+    Repond en renvoyant un dictionnaire sans aucun autres commentaires.
+
+    Voici la description de l'annonce en question: 
+
+    {{ {description} }}
+    """
+
+    response = llm.invoke(question)
+
+    return response
