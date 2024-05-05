7c655db6aa229df1b920f3ab077243de6dd14b0a
matthieuneau <matthieu@upflowy.com>
2024-05-05 16:25:44 +1000

    work on making pap headless

--- scraping/pap/main.py
+++ scraping/pap/main.py
@@ -10,26 +10,31 @@ from saveToDatabase import save_to_database
 
 # Setup Chrome options for undetected_chromedriver
 options = uc.ChromeOptions()
-# options.add_argument("--headless")
+options.add_argument("--headless")
 options.add_argument("--incognito")
+options.add_argument("--disable-blink-features=AutomationControlled")
+options.add_argument(
+    "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+)
+
 
 driver = uc.Chrome(options=options)
 
 data_collected = pd.DataFrame()
 
-pages_to_scrape = list(range(1, 5))
+pages_to_scrape = list(range(1, 2))
 
 ### SCRAPING LOOP
 
 for page_num in tqdm(pages_to_scrape, desc="Scraping page"):
     url = f"https://www.pap.fr/annonce/location-appartement-maison-{page_num}"
     driver.get(url)
-    time.sleep(2)
+    time.sleep(6)
 
     url_list = driver.find_elements(By.CSS_SELECTOR, "a.item-thumb-link")
     url_list = [item.get_attribute("href") for item in url_list]
-    # url_list = url_list[:1]
-    # print(f"url_list: {url_list}")
+    url_list = url_list[:2]
+    print(f"url_list: {url_list}")
 
     # Create a manual tqdm progress bar for the inner loop
     pbar = tqdm(
