c6407900a76fecbbcf14c3dc63eb201002a086a8
koffipynb <hennecon.lucas@gmail.com>
2024-05-11 00:27:56 +1000

    airbnb-oxylab using JSON file

--- scraping/leboncoin-oxylab/main.py
+++ scraping/leboncoin-oxylab/main.py
@@ -13,13 +13,13 @@ if __name__ == "__main__":
     #handler(None, None)
 
     #Download the hmtl of the web page:
-    # html_page = fetch_html_with_oxylab("https://www.seloger.com/annonces/locations/appartement/croissy-sur-seine-78/centre-ville/210212077.htm")
-    # with open("C:/Users/hennecol/Documents/safeflat/scraping/seloger-oxylab/annonces/annonce2.html", "w", encoding="utf-8") as file:
+    # html_page = fetch_html_with_oxylab("https://www.leboncoin.fr/ad/locations/2689785455")
+    # with open("/Users/lucashennecon/Documents/Mission JE/safeflat/scraping/leboncoin-oxylab/annonces/annonce2.html", "w", encoding="utf-8") as file:
     #     file.write(html_page)
 
     # Find urls:
-    # retrieve_urls("https://www.seloger.com/list.htm?projects=1&types=2,1&places=[{%22divisions%22:[2238]}]&mandatorycommodities=0&privateseller=1&enterprise=0&qsVersion=1.0&m=search_refine-redirection-search_results")
+    # retrieve_urls("https://www.leboncoin.fr/recherche?category=10&owner_type=private")
 
     # Parse a page:
-    data =scrape_ad('https://www.seloger.com/annonces/locations/appartement/livry-gargan-93/sully-vauban/212607781.htm')
+    data =scrape_ad('https://www.leboncoin.fr/ad/locations/2681655531')
     print(data)
