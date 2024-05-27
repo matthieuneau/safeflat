10e48ddd3c7a91c7d8ae459ea604f8ff469fe98d
matthieuneau <matthieu@upflowy.com>
2024-05-27 23:23:10 +1000

    refactor pap and seloger

---
+++ archive-refactoring/seloger-retrieve-urls/utils.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import os
+from langchain_openai import OpenAI
+import requests
+from bs4 import BeautifulSoup
+import json
+import re
+from preprocessing import *
---
+++ safeflat-sam-app/retrieveUrls/utils.py
@@ -0,0 +1,54 @@
+import requests
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+def fetch_html_with_oxylab(page_url: str) -> str:
+    username = "safeflat"
+    password = "saaj098KLN++"
+
+    proxies = {
+        "http": f"http://{username}:{password}@unblock.oxylabs.io:60000",
+        "https": f"http://{username}:{password}@unblock.oxylabs.io:60000",
+    }
+
+    response = requests.request(
+        "GET",
+        page_url,
+        verify=False,  # Ignore the certificate
+        proxies=proxies,
+    )
+
+    return response.text
+
+
+### TO BE EDITED ###
+def read_from_database(query: str) -> pd.DataFrame:
+    db_config = {
+        "host": "safeflat-scraping-data.cls8g8ie67qg.us-east-1.rds.amazonaws.com",
+        "port": 3306,
+        "user": "admin",
+        "password": "SBerWIyVxBu229rGer6Z",
+        "database": "scraping",
+    }
+
+    # Creating a connection string for SQLAlchemy
+    connection_string = f'mysql+pymysql://{db_config["user"]}:{db_config["password"]}@{db_config["host"]}:{db_config["port"]}/{db_config["database"]}'
+
+    engine = create_engine(connection_string)
+
+    df = pd.read_sql_query(query, con=engine)
+
+    return df
+
+
+### TO BE EDITED ###
+def remove_already_scraped_urls(urls: list) -> list:
+    query = "select url from pap"
+    scraped_urls_df = read_from_database(query)["url"]
+    print("scraped_urls_df: ", scraped_urls_df)
+    scraped_urls_list = scraped_urls_df.to_list()
+    print("scrapped_urls_list: ", scraped_urls_list)
+    urls = [url for url in urls if url not in scraped_urls_list]
+
+    return urls
